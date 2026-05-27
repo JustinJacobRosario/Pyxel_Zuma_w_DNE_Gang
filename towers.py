73a23458c157fec7139ce8bc3623ec8c1c1237b3
EnzoO <enzoeoh@protonmail.com>
2026-05-27 21:08:55 +0800

    changed: tower because i dont know how bullets work

--- towers.py
+++ towers.py
@@ -1,24 +1,41 @@
 from abc import ABC, abstractmethod
-from bullets import * # temp, wildcard import bad
-# from enemies import Color
+from bullets import Bullet, OrangeBullet, RedBullet, BlueBullet
+from enemies import Color
 import random
 
 class Tower(ABC):
-    def __init__(self, pos_col, pos_row, color_type, bullet_type):
+    _exp_cost: int = 0
+    _range: float = 0.0
+    _bullet_colors: list[Color] = []
+
+    def __init__(self, pos_col, pos_row, bullet_type):
         self._fire_rate: float = 0.5 # bullets / second
-        self._color: Color = color_type
         self._bullet: Bullet = bullet_type # should control bullet speed and hits
         self._col: float = float(pos_col) # defined per instance
         self._row: float = float(pos_row) # defined per instance
         self._upgraded: bool = False
-        self._exp_cost: int = 0
-        self._range: int = 0
-		
-    def get_exp_cost(self) -> int:
+	
+    @property
+    def col(self) -> float: # req for pos drawing and range checking
+        return self._col
+    
+    @property
+    def row(self) -> float: # req for pos drawing and range checking
+        return self._row
+    
+    @property
+    def fire_rate(self) -> float: # req for shooting cooldown
+        return self._fire_rate
+    
+    @property
+    def bullet(self) -> Bullet: # req for shooting and drawing
+        return self._bullet
+    
+    @property
+    def exp_cost(self) -> int: # req for placing
         return self._exp_cost
-
-    def get_range(self) -> int:
-        return self._range
+    
+    # --
 
     def pick_bullet_color(self) -> Color:
         return random.choice(self._bullet_colors)
@@ -31,6 +48,16 @@ class Tower(ABC):
 
     def shoot(self, target) -> Bullet:
         color = self.pick_bullet_color()
+        bullet = self._bullet(color)
+        bullet.x = self._col
+        bullet.y = self._row
+        bullet.is_used = False
+        return bullet
 		
 class RainbowTower(Tower): # this is the basic tower i just couldnt think of a better name
-	...
+	_exp_cost = 5
+    _range = 5.0
+    _bullet_colors = [Color.Orange, Color.Red, Color.Blue]
+
+    def __init__(self, pos_col, pos_row):
+        super().__init__(pos_col, pos_row)
